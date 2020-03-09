90f2d756f8e6ad6f13b319c5bcb425f7c411cac4
Viktor <viktorku@kth.se>
2020-03-09 11:37:19 +0100

    Start of filter independant ROS-node.

---
+++ include/kalman_node.h
@@ -0,0 +1,67 @@
+#pragma once
+
+#include <string>
+
+#include "ros/ros.h"
+
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/transform_broadcaster.h"
+
+#include "sensor_msgs/Imu.h"
+#include "geometry_msgs/PoseStamped.h"
+
+#include "iekf.h"
+#include "iekf_types.h"
+
+namespace invariant
+{
+
+class KalmanNode
+{
+private:
+    ros::NodeHandle& m_nh;
+    ros::NodeHandle& m_nh_private;
+
+    ros::Subscriber m_imu_sub;
+    ros::Subscriber m_mocap_sub;
+
+    ros::Publisher m_pose_pub;
+    ros::Publisher m_velocity_pub;
+
+    tf2_ros::TransformBroadcaster m_tf_broadcaster;
+
+    const std::string m_base_frame;
+    const std::string m_map_frame;
+
+    ros::Timer m_timer;
+
+    IEKF m_iekf_filter;
+
+    ros::Time m_previous_imu_time;
+
+    bool m_imu_recieved = false;
+    Vector3 m_imu_acc = Vector3::Zero();
+    Vector3 m_imu_ang_vel = Vector3::Zero();
+
+    bool m_mocap_recieved = false;
+    Rotation m_mocap_R = Rotation::Identity();
+    Vector3 m_mocap_pos = Vector3::Zero();
+
+public:
+    KalmanNode(ros::NodeHandle& nh, ros::NodeHandle& nh_private);
+
+    void start();
+
+private:
+    void initialise_iekf_filter();
+
+    void timer_cb(const ros::TimerEvent& e);
+    void imu_cb(const sensor_msgs::Imu& msg);
+    void mocap_cb(const geometry_msgs::PoseStamped& msg);
+
+    void publish_tf(const ros::Time& stamp);
+    void publish_pose(const ros::Time& stamp);
+    void publish_velocity(const ros::Time& stamp);
+};
+
+}
---
+++ src/kalman_node.cpp
@@ -0,0 +1,158 @@
+#include "kalman_node.h"
+
+#include <string>
+
+#include <ros/ros.h>
+
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_eigen/tf2_eigen.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+
+#include "sensor_msgs/Imu.h"
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/TransformStamped.h"
+
+#include "iekf.h"
+#include "iekf_types.h"
+#include "iekf_tf2_conversions.h"
+
+#include "ros_utils.h"
+
+namespace invariant
+{
+
+KalmanNode::KalmanNode(ros::NodeHandle& nh, ros::NodeHandle& nh_private)
+    : m_nh(nh)
+    , m_nh_private(nh_private)
+    , m_base_frame( [&nh_private]() -> std::string { return nh_private.param<std::string>("base_frame", "base_link"); }() )
+    , m_map_frame(  [&nh_private]() -> std::string { return nh_private.param<std::string>("map_frame", "map"); }() )
+{
+    m_imu_sub       = m_nh.subscribe("imu", 10, KalmanNode::imu_cb, this);
+    m_mocap_sub     = m_nh.subscribe("mocap/pose", 10, KalmanNode::mocap_cb, this);
+    m_pose_pub      = m_nh.advertise<geometry_msgs::PoseStamped>("pose_filtered", 10);
+    m_velocity_pub  = m_nh.advertise<geometry_msgs::PointStamped>("vel_filtered", 10);
+
+    const double frequency = m_nh_private.param<double>("frequency", 10.0);
+    m_timer = m_nh.createTimer(1.0/frequency, &KalmanNode::timer_cb, this, false, false);
+
+    ros_utils::wait_for_message<sensor_msgs::Imu>(m_imu_sub);
+
+    // TODO: Measure accelerometer bias.
+    initialise_iekf_filter();
+
+    ROS_DEBUG_STREAM("State initialised to:" 
+                    << "\nR = \n" << m_iekf_filter.get_rot()
+                    << "\npos = \n" << m_iekf_filter.get_pos()
+                    << "\nvel = \n" << m_iekf_filter.get_vel());
+}
+
+void KalmanNode::start()
+{
+    m_previous_imu_time = ros::Time::now();
+    m_imu_recieved = false;
+    m_mocap_recieved = false;
+    m_timer.start();
+    ROS_INFO("Timer started!");
+}
+
+void KalmanNode::initialise_iekf_filter()
+{
+    // TODO: Initilise from launch parameters.
+    const Vector3 p0 = Vector3::Zero();
+    const Vector3 v0 = Vector3::Zero();
+    const Rotation R0 = Rotation::Identity();
+    const Covariance<9> P0 = Covariance<9>::Identity() * 0.1;
+
+    m_iekf_filter = IEKF(R0, p0, v0, P0);
+
+    return;
+}
+
+void KalmanNode::timer_cb(const ros::TimerEvent& e)
+{
+    if (m_imu_recieved)
+    {
+        // TODO: Create imu msg queue and calculate dt from imu timestamps.
+        const double dt = (e.current_real - m_previous_imu_time).toSec();
+        m_iekf_filter.predict(dt, m_imu_acc, m_imu_ang_vel);
+        m_previous_imu_time = e.current_real;
+        m_imu_recieved = false;
+    }
+    if (m_mocap_recieved)
+    {
+        m_iekf_filter.mocap_update(m_mocap_R, m_mocap_pos);
+        m_mocap_recieved = false;
+    }
+
+    publish_tf(e.current_real);
+    publish_pose(e.current_real);
+    publish_velocity(e.current_real);
+}
+
+void KalmanNode::imu_cb(const sensor_msgs::Imu& msg)
+{
+    tf2::fromMsg(msg.linear_acceleration, m_imu_acc);
+    tf2::fromMsg(msg.angular_velocity, m_imu_ang_vel);
+    m_imu_recieved = true;
+}
+    
+void KalmanNode::mocap_cb(const geometry_msgs::PoseStamped& msg)
+{
+    tf2::fromMsg(msg.pose.orientation, m_mocap_R);
+    tf2::fromMsg(msg.pose.position, m_mocap_pos);
+    m_mocap_recieved = true;
+}
+
+void KalmanNode::publish_tf(const ros::Time& stamp)
+{
+    geometry_msgs::TransformStamped tf;
+
+    tf.header.stamp     = stamp;
+    tf.header.frame_id  = m_map_frame;
+    tf.child_frame_id   = m_base_frame;
+
+    tf2::toMsg(m_iekf_filter.get_rot(), tf.transform.rotation);
+    tf2::toMsg(m_iekf_filter.get_pos(), tf.transform.translation);
+
+    m_tf_broadcaster.sendTransform(tf);
+}
+
+void KalmanNode::publish_pose(const ros::Time& stamp)
+{
+    geometry_msgs::PoseStamped pose;
+
+    pose.header.stamp     = stamp;
+    pose.header.frame_id  = m_map_frame;
+    pose.pose.position    = tf2::toMsg(m_iekf_filter.get_pos());
+    pose.pose.orientation = tf2::toMsg(m_iekf_filter.get_quat());
+
+    m_pose_pub.publish(pose);
+}
+
+void KalmanNode::publish_velocity(const ros::Time& stamp)
+{
+    geometry_msgs::PointStamped vel;
+
+    vel.header.stamp    = stamp;
+    vel.header.frame_id = m_map_frame;
+    vel.point           = tf2::toMsg(m_iekf_filter.get_vel());
+
+    m_velocity_pub.publish(vel);
+}
+
+}
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "kalman_node");
+    ros::NodeHandle nh("");
+    ros::NodeHandle nh_private("~");
+
+    ROS_INFO("Initialising node...");
+    invariant::KalmanNode node(nh, nh_private);
+    ROS_INFO("Node initialisation done.");
+
+    node.start();
+    ros::spin();
+}
