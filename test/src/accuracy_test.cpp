4083cfc2b47431e15d41e63ee19f1394ecbb5d94
kullken <viktorku@kth.se>
2020-09-29 14:47:43 +0200

    Use RTTI instead of virtual func for sensor events

--- test/src/accuracy_test.cpp
+++ test/src/accuracy_test.cpp
@@ -1,9 +1,11 @@
 #include "accuracy_test.h"
 
 #include <algorithm>
+#include <exception>
 #include <iterator>
 #include <memory>
 #include <numeric>
+#include <typeinfo>
 #include <vector>
 
 #include <ros/time.h>
@@ -38,39 +40,34 @@ double dist(const ugl::UnitQuaternion& a, const ugl::UnitQuaternion& b)
     return a.angularDistance(b);
 }
 
-template<typename FilterType>
-class Event
+class SensorEvent
 {
 public:
-    virtual ~Event() = default;
+    virtual ~SensorEvent() = default;
 
-    const ros::Time& time() const { return m_time; }
-
-    virtual void update_filter(FilterType& filter) const = 0;
+    const auto& time() const { return m_time; }
 
 protected:
-    Event(const ros::Time& time) : m_time(time) {}
+    SensorEvent(const ros::Time& time) : m_time(time) {}
 
 private:
     ros::Time m_time;
 };
 
-template<typename FilterType>
-class ImuEvent: public Event<FilterType>
+class ImuEvent: public SensorEvent
 {
 public:
     ImuEvent(const ros::Time& time, double dt, ugl::Vector3 acc, ugl::Vector3 rate)
-        : Event<FilterType>(time)
+        : SensorEvent(time)
         , m_dt(dt)
         , m_acc(acc)
         , m_rate(rate)
     {
     }
 
-    void update_filter(FilterType& filter) const override
-    {
-        filter.predict(m_dt, m_acc, m_rate);
-    }
+    const auto& dt() { return m_dt; }
+    const auto& acc() { return m_acc; }
+    const auto& rate() { return m_rate; }
 
 private:
     double m_dt;
@@ -78,30 +75,24 @@ private:
     ugl::Vector3 m_rate;
 };
 
-template<typename FilterType>
-class MocapEvent: public Event<FilterType>
+class MocapEvent: public SensorEvent
 {
 public:
     MocapEvent(const ros::Time& time, ugl::lie::Pose pose)
-        : Event<FilterType>(time)
+        : SensorEvent(time)
         , m_pose(pose)
     {
     }
 
-    void update_filter(FilterType& filter) const override
-    {
-        filter.mocap_update(m_pose);
-    }
+    const auto& pose() { return m_pose; }
 
 private:
     ugl::lie::Pose m_pose;
 };
 
-template<typename FilterType>
-using EventPtr = std::shared_ptr<Event<FilterType>>;
+using EventPtr = std::shared_ptr<SensorEvent>;
 
-template<typename FilterType>
-using Events = std::vector<EventPtr<FilterType>>;
+using Events = std::vector<EventPtr>;
 
 struct Estimate
 {
@@ -121,12 +112,11 @@ struct Estimate
 /// @param imu the IMU sensor model used to generate IMU data
 /// @param mocap the motion-capture sensor model used to generate motion-capture data
 /// @return Vector of data-events
-template<typename FilterType>
-Events<FilterType> generate_events(const ugl::trajectory::Trajectory& trajectory,
-                                   const ImuSensorModel& imu,
-                                   const MocapSensorModel& mocap)
+Events generate_events(const ugl::trajectory::Trajectory& trajectory,
+                       const ImuSensorModel& imu,
+                       const MocapSensorModel& mocap)
 {
-    Events<FilterType> events{};
+    Events events{};
 
     const ros::Time start_time{0.0};
     const ros::Time end_time{trajectory.duration()};
@@ -142,17 +132,17 @@ Events<FilterType> generate_events(const ugl::trajectory::Trajectory& trajectory
         if (time >= next_imu_time)
         {
             const double t = next_imu_time.toSec();
-            events.push_back(std::make_shared<ImuEvent<FilterType>>(next_imu_time, imu.period(),
-                                                                    imu.get_accel_reading(t, trajectory),
-                                                                    imu.get_gyro_reading(t, trajectory)));
+            events.push_back(std::make_shared<ImuEvent>(next_imu_time, imu.period(),
+                                                        imu.get_accel_reading(t, trajectory),
+                                                        imu.get_gyro_reading(t, trajectory)));
             next_imu_time += imu_period;
         }
 
         if (time >= next_mocap_time)
         {
             const double t = next_mocap_time.toSec();
-            events.push_back(std::make_shared<MocapEvent<FilterType>>(next_mocap_time,
-                                                                      mocap.get_pose_reading(t, trajectory)));
+            events.push_back(std::make_shared<MocapEvent>(next_mocap_time,
+                                                          mocap.get_pose_reading(t, trajectory)));
             next_mocap_time += mocap_period;
         }
     }
@@ -160,12 +150,34 @@ Events<FilterType> generate_events(const ugl::trajectory::Trajectory& trajectory
     return events;
 }
 
+/// @brief Updates a filter with a sensor event
+/// @param filter [in,out] the filter to update
+/// @param event [in] the sensor event
+template<typename FilterType>
+void update_filter(FilterType& filter, const SensorEvent& event)
+{
+    if (typeid(event) == typeid(ImuEvent))
+    {
+        auto imu_event = dynamic_cast<const ImuEvent&>(event);
+        filter.predict(imu_event.dt(), imu_event.acc(), imu_event.rate());
+        return;
+    }
+    else if (typeid(event) == typeid(MocapEvent))
+    {
+        auto mocap_event = dynamic_cast<const MocapEvent&>(event);
+        filter.mocap_update(mocap_event.pose());
+        return;
+    }
+
+    throw std::logic_error("Sensor event could not be downcasted to any known sensor type.");
+}
+
 /// @brief Run filter on collection of data-events
 /// @param filter the filter to use (copied before use)
 /// @param events the collection of events (assumed chronologically ordered)
 /// @return Vector of timestamps and estimated states
 template<typename FilterType>
-std::vector<Estimate> run_filter(FilterType filter, const Events<FilterType>& events)
+std::vector<Estimate> run_filter(FilterType filter, const Events& events)
 {
     std::vector<Estimate> estimates{};
 
@@ -178,7 +190,7 @@ std::vector<Estimate> run_filter(FilterType filter, const Events<FilterType>& ev
     {
         while (event_it != std::cend(events) && (*event_it)->time() <= time)
         {
-            (*event_it)->update_filter(filter);
+            update_filter(filter, **event_it);
             ++event_it;
         }
         estimates.emplace_back(time, filter.get_state());
@@ -191,7 +203,6 @@ std::vector<Estimate> run_filter(FilterType filter, const Events<FilterType>& ev
 /// @param estimates the estimates from the filter
 /// @param trajectory the ground truth trajectory
 /// @return Result of the comparision
-template<typename FilterType>
 Result calculate_result(const ugl::trajectory::Trajectory& trajectory, const std::vector<Estimate>& estimates)
 {
     Result result{};
@@ -230,9 +241,9 @@ Result IekfTestSuite::compute_accuracy()
     filter_.set_pos(trajectory_.get_position(0.0));
     filter_.set_vel(trajectory_.get_velocity(0.0));
     filter_.set_rot(trajectory_.get_rotation(0.0));
-    auto events = generate_events<invariant::IEKF>(trajectory_, imu_, mocap_);
+    auto events = generate_events(trajectory_, imu_, mocap_);
     auto estimates = run_filter<invariant::IEKF>(filter_, events);
-    return calculate_result<invariant::IEKF>(trajectory_, estimates);
+    return calculate_result(trajectory_, estimates);
 }
 
 Result MekfTestSuite::compute_accuracy()
@@ -240,9 +251,9 @@ Result MekfTestSuite::compute_accuracy()
     filter_.set_pos(trajectory_.get_position(0.0));
     filter_.set_vel(trajectory_.get_velocity(0.0));
     filter_.set_rot(trajectory_.get_rotation(0.0));
-    auto events = generate_events<mekf::MEKF>(trajectory_, imu_, mocap_);
+    auto events = generate_events(trajectory_, imu_, mocap_);
     auto estimates = run_filter<mekf::MEKF>(filter_, events);
-    return calculate_result<mekf::MEKF>(trajectory_, estimates);
+    return calculate_result(trajectory_, estimates);
 }
 
 std::ostream& operator<<(std::ostream& os, const Result& result)
