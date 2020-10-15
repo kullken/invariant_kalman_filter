0f618197dfdc95d2e075c792b51e7660ec27471c
kullken <viktorku@kth.se>
2020-10-15 11:44:00 +0200

    Use structured numpy arrays when reading csv data

--- test/scripts/plot_results.py
+++ test/scripts/plot_results.py
@@ -5,6 +5,32 @@ import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 
+vec3_type = np.dtype([
+    ("x", float),
+    ("y", float),
+    ("z", float),
+])
+
+quat_type = np.dtype([
+    ("x", float),
+    ("y", float),
+    ("z", float),
+    ("w", float),
+])
+
+csv_data_type = np.dtype([
+    ("time",      float),
+    ("pos_err",   float),
+    ("vel_err",   float),
+    ("rot_err",   float),
+    ("pos_pred",  vec3_type),
+    ("vel_pred",  vec3_type),
+    ("quat_pred", quat_type),
+    ("pos_true",  vec3_type),
+    ("vel_true",  vec3_type),
+    ("quat_true", quat_type),
+])
+
 def create_arg_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -41,13 +67,13 @@ def plot_position(times, pos_pred, pos_true, descr=""):
     figure = plt.figure()
     ax = figure.add_subplot(111)
 
-    ax.plot(times, pos_pred[0], label="x - Predicted")
-    ax.plot(times, pos_pred[1], label="y - Predicted")
-    ax.plot(times, pos_pred[2], label="z - Predicted")
+    ax.plot(times, pos_pred["x"], label="x - Predicted")
+    ax.plot(times, pos_pred["y"], label="y - Predicted")
+    ax.plot(times, pos_pred["z"], label="z - Predicted")
 
-    ax.plot(times, pos_true[0], "--", label="x - True")
-    ax.plot(times, pos_true[1], "--", label="y - True")
-    ax.plot(times, pos_true[2], "--", label="z - True")
+    ax.plot(times, pos_true["x"], "--", label="x - True")
+    ax.plot(times, pos_true["y"], "--", label="y - True")
+    ax.plot(times, pos_true["z"], "--", label="z - True")
 
     ax.set_xlabel("Time [s]")
     ax.set_ylabel("Position [m]")
@@ -63,22 +89,9 @@ if __name__ == "__main__":
     with open(file_path, 'r') as f:
         description = f.readline()[2:]
 
-    data = np.loadtxt(file_path, skiprows=2, unpack=True)
+    data = np.loadtxt(file_path, dtype=csv_data_type, skiprows=2)
 
-    times = data[0]
-    pos_err = data[1]
-    vel_err = data[2]
-    rot_err = data[3]
-
-    pos_pred = data[4:7]
-    vel_pred = data[7:10]
-    quat_pred = data[10:14]
-
-    pos_true = data[14:17]
-    vel_true = data[17:20]
-    quat_true = data[20:24]
-
-    plot_error(times, pos_err, vel_err, rot_err, description)
-    plot_position(times, pos_pred, pos_true, description)
+    plot_error(data["time"], data["pos_err"], data["vel_err"], data["rot_err"], description)
+    plot_position(data["time"], data["pos_pred"], data["pos_true"], description)
 
     plt.show()
